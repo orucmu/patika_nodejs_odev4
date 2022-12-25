55d1ea18cfda6936f6596dd51b3ec816ee2ba743
orucmu <mustafaoruc720@gmail.com>
2022-12-25 22:16:41 +0300

    second

--- index.js
+++ index.js
@@ -32,33 +32,3 @@ fs.unlink('employee.json', (err) => {
     console.log("Employees file was deleted.")
 })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// fs.writeFile('employees.json', '{"name": "Employee 1 Name", "salary": 2000},', "utf-8", (err) => {
-//     if (err) console.log(err);
-//     console.log('Employees file created.')
-// })
-
-// // fs.readFile('employees.json', 'utf-8', (err, data) => {
-// //     if (err) console.log(err);
-// //     console.log(data);
-// //     console.log("Employees file was read ")
-// // })
-
-// fs.appendFile('employees.json', '\n{"name": "Employee 2 Name", "salary": "2200"}', 'utf-8', (err) => {
-//     if (err) console.log(err);
-//     console.log("A new employee has been added.")
-// })
